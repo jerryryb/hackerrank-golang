b376b4022a19e7245853c6f40d4d0bf2a2a744f0
Jaroslaw Rybski <r_jaro@o2.pl>
2017-01-26 21:11:36 +0100

    solution for https://www.hackerrank.com/contests/projecteuler/challenges/euler007

---
+++ projecteuler/euler007.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+
+func main() {
+    t := 0
+    fmt.Scanf("%d", &t)
+    for t > 0 {
+        t--
+        n := 0
+        primes := []int{2,3}
+        fmt.Scanf("%d", &n)
+        if len(primes) >= n {
+            fmt.Printf("%d\n", primes[n-1])
+        }else {
+            counter := 4
+            for len(primes) < n {
+                if counter % 2 != 0 && counter % 3 != 0 {
+                    temp := 4
+                    for temp * temp <= counter {
+                        if counter % temp == 0 {
+                            break
+                        }
+                        temp++
+                    }
+                    if temp * temp > counter {
+                        primes = append(primes, counter)
+                    }
+                }
+                counter++
+            }
+            fmt.Printf("%d\n", primes[n-1])
+        }
+    }
+}
