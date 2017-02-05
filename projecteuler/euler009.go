8504fe514ea1a60a2a15c1e4e596c4202dccc2e3
Jaroslaw Rybski <r_jaro@o2.pl>
2017-02-05 17:34:08 +0100

    performance fix formula - solution for https://www.hackerrank.com/contests/projecteuler/challenges/euler009

--- projecteuler/euler009.go
+++ projecteuler/euler009.go
@@ -8,23 +8,15 @@ func main() {
         t--
         n := 0
         fmt.Scanf("%d", &n)
+        
         result := -1
-        for a := 1; a < n-2; a++ {
-            for b := a+1; b < n-1; b++ {
-                if a + b > n {
-                    break
-                }
+        for a := 1; a < n; a++ {
 
-                c := n - a - b
+            b := ((n-a)*(n-a)-(a*a))/(2 * (n-a))
+            c := n-b-a
 
-                if a + b + c > n {
-                    break
-                }
-                if a + b + c == n {
-                    if a*a + b*b == c*c {
-                        result = a * b * c
-                    }
-                }
+            if a*a+b*b == c*c && b > a && c > b {
+                result = a * b * c
             }
         }
         fmt.Printf("%d\n", result)
