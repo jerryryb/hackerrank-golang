da05910775ec3cdac00803ed20a4e8f22e57fb5d
Jaroslaw Rybski <r_jaro@o2.pl>
2017-02-05 17:36:53 +0100

    solution for https://www.hackerrank.com/contests/projecteuler/challenges/euler010

---
+++ projecteuler/euler010.go
@@ -0,0 +1,45 @@
+package main
+import "fmt"
+
+func generatePrimes(n int64, primes []int64) []int64 {
+	counter := primes[len(primes)-1] + 1
+	for true {
+		if n < primes[len(primes)-1] {
+			break
+		}
+		prime := true
+		for _, elem := range primes {
+			if counter % elem == 0 {
+				prime = false
+				break
+			}
+		}
+		if prime {
+			primes = append(primes, counter)
+		}
+		counter++
+	}
+	return primes
+}
+
+func main() {
+    t := 0
+    fmt.Scanf("%d", &t)
+    primes := []int64{2,3}
+    for t > 0 {
+        t--
+        n := int64(0)
+        fmt.Scanf("%d", &n)
+        if n > primes[len(primes)-1] {
+            primes = generatePrimes(n, primes)
+        }
+        result := int64(0)
+        for _, elem := range primes {
+            if elem > n {
+                break
+            }
+            result += elem
+        }
+        fmt.Println(result)
+    }
+}
